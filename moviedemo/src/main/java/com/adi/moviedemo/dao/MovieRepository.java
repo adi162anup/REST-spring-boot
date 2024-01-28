35849e4de0068a94231036dfd5b16e4afe423022
adi162anup <adithyaanup2002@gmail.com>
2024-01-28 15:12:39 +0530

    Changed default endpoint

--- moviedemo/src/main/java/com/adi/moviedemo/dao/MovieRepository.java
+++ moviedemo/src/main/java/com/adi/moviedemo/dao/MovieRepository.java
@@ -2,7 +2,10 @@ package com.adi.moviedemo.dao;
 
 import com.adi.moviedemo.entity.Movie;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@RepositoryRestResource(path="classics")
 public interface MovieRepository extends JpaRepository<Movie, Integer> {
 
 }
