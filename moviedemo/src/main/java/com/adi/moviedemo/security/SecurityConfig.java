c47338c4b9ffe9fe040aa728ea16db1ec250d267
adi162anup <adithyaanup2002@gmail.com>
2024-03-03 20:07:23 +0530

    Added security for accessing urls

--- moviedemo/src/main/java/com/adi/moviedemo/security/SecurityConfig.java
+++ moviedemo/src/main/java/com/adi/moviedemo/security/SecurityConfig.java
@@ -2,10 +2,15 @@ package com.adi.moviedemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration()
 public class SecurityConfig {
@@ -21,16 +26,36 @@ public class SecurityConfig {
 
         UserDetails rohit = User.builder()
                 .username("rohit")
-                .password("sharma123")
+                .password("{noop}sharma123")
                 .roles("EMPLOYEE","MANAGER")
                 .build();
 
         UserDetails rahul = User.builder()
                 .username("rahul")
-                .password("dravid123")
+                .password("{noop}dravid123")
                 .roles("EMPLOYEE","MANAGER","ADMIN")
                 .build();
 
         return new InMemoryUserDetailsManager(sarfaraz,rohit,rahul);
+
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET,"/api/theatres").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET,"/api/theatres/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.POST,"/api/theatres").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/api/theatres").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/api/theatres/**").hasRole("ADMIN")
+        );
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+        return http.build();
     }
 }
